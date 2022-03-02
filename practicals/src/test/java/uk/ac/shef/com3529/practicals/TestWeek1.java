c2c3aeeedd8b46e577a39fd6d3fa6cd1a9e5f5f6
TheDarthMole <27959011+TheDarthMole@users.noreply.github.com>
2022-03-02 13:11:57 +0000

    Initial commit

---
+++ practicals/src/test/java/uk/ac/shef/com3529/practicals/TestWeek1.java
@@ -0,0 +1,88 @@
+package uk.ac.shef.com3529.practicals;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static uk.ac.shef.com3529.practicals.Week1.*;
+
+public class TestWeek1 {
+
+    // findLast
+    // --------
+    // (a) Identify the fault
+    @Test
+    public void testFindLastFails() {
+    }
+
+    // (b) A JUnit test case that does not execute the fault
+    @Test
+    public void testFindLastDoesNotExecuteFault() {
+    }
+
+    // (c) A JUnit test case that executes the fault but does not infect the program state
+    @Test
+    public void testFindLastExecutesFaultDoesNotInfectState() {
+    }
+
+    // (d) Infection but no failure
+    @Test
+    public void testFindLastInfectionNoFailure() {
+    }
+
+    // countPositive
+    // --------
+    // (a) Identify the fault
+    @Test
+    public void testCountPositiveFails() {
+    }
+    // (b) A JUnit test case that does not execute the fault
+    @Test
+    public void testCountPositiveDoesNotExecuteFault() {
+    }
+    // (c) A JUnit test case that executes the fault but does not infect the program state
+    @Test
+    public void testCountPositiveExecutesFaultDoesNotInfectState() {
+    }
+    // (d) Infection but no failure
+    @Test
+    public void testCountPositiveInfectionNoFailure() {
+    }
+
+    // lastZero
+    // --------
+    // (a) Identify the fault
+    @Test
+    public void testLastZeroFails() {
+    }
+    // (b) A JUnit test case that does not execute the fault
+    @Test
+    public void testLastZeroDoesNotExecuteFault() {
+    }
+    // (c) A JUnit test case that executes the fault but does not infect the program state
+    @Test
+    public void testLastZeroExecutesFaultDoesNotInfectState() {
+    }
+    // (d) Infection but no failure
+    @Test
+    public void testLastZeroInfectionButNoFailure() {
+    }
+
+    // oddOrPos
+    // --------
+    // (a) Identify the fault
+    @Test
+    public void testOddOrPosFails() {
+    }
+    // (b) A JUnit test case that does not execute the fault
+    @Test
+    public void testOddOrPosDoesNotExecuteFault() {
+    }
+    // (c) A JUnit test case that executes the fault but does not infect the program state
+    @Test
+    public void testOddOrPosExecutesFaultDoesNotInfectState() {
+    }
+    // (d) Infection but no failure
+    @Test
+    public void testOddOrPosInfectionButNoFailure() {
+    }
+}
